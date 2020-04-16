b0bec44869fd4a048340cb0bda0860ac5ea7c24b
Justin Glenn Smith <justin.smith@fundingcircle.com>
2020-04-16 11:08:43 -0700

    function to create an exponential curve

--- 6-scheme-4-2020/mksco.scm
+++ 6-scheme-4-2020/mksco.scm
@@ -17,6 +17,15 @@
   (max-hz note-max-hz set-note-max-hz!)
   (min-hz note-min-hz set-note-min-hz!))
 
+(define-record-type <curve>
+  (make-curve description
+              n size data)
+  curve?
+  (description curve-description)
+  (n curve-n)
+  (size curve-size)
+  (data curve-data))
+
 (define (emit-note note)
   (format #t "i~a ~a ~a ~a  "
           (note-instrument note)
@@ -28,10 +37,31 @@
           (note-max-hz note))
   (format #t "; ~a\n" (note-description note)))
 
+(define (emit-curve curve)
+  ;; a breakpoint is a (curve-time . final-value) cons pair
+  ;; the total duration will be normalized from total curve times to the size
+  (let* ((data (curve-data curve))
+         (init (car data))
+         (breakpoints (cdr data))
+         (total (apply + 0.0 (map car breakpoints)))
+         (norm (/ (curve-size curve) total)))
+    (format #t "\nf ~a 0 ~a 5 ~a"
+            (curve-n curve) (curve-size curve) init)
+    (map (lambda (bp)
+           (format #t " ~a ~a"
+                   (inexact->exact (floor (* norm (car bp))))
+                   (cdr bp)))
+         breakpoints)
+    (format #t " ; ~a\n" (curve-description curve))))
+
 ;; Ideas:
 ; create tables for curves for each K param (and read from them in the instrument)
 ; paramaterize more controls
 ; filter (resonant filters...)
+
+(define curve1
+  (make-curve "a simple curve" 1 1024 '(0.1 (1 . 1.0) (5 . 0.1))))
+
 (define e1
   (let ((max-hz 1)
         (min-hz 1.1))
@@ -39,7 +69,8 @@
                min-hz
                max-hz)))
 
-(define (gen-args)
+(define (gen-sco)
+   (emit-curve curve1)
    (emit-note e1))
 
-(gen-args)
+(gen-sco)
