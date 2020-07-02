7f2b4d83063ea1607731ab7c4c2c1475cec0ce17
Justin Glenn Smith <justin.smith@fundingcircle.com>
2020-07-02 08:21:59 -0700

    a starting point for clean multimethod dispatch

--- 6-scheme-4-2020/scm/noisesmith/oo.scm
+++ 6-scheme-4-2020/scm/noisesmith/oo.scm
@@ -20,3 +20,28 @@
      (lambda (. args)
        (_thing_match (car args)
                      k v kvs ...)))))
+
+(define (create-table)
+  ;;; ...
+  )
+
+(define (get-from-table t k)
+  ;;; ...
+  )
+
+(define (put-in-table t k v)
+  ;;; ...
+  )
+
+;; TODO: this is almost it - it is close to clojure's defmulti, without the
+;; binding part, and without the heirarchic lookup of dispatch built in
+;; the inheriting dispatch can be a wrapper for the dispatch-lambda
+;; would a separate version that only dispatches on the first arg's type be an optimization?
+(define (defmulti dispatch-lambda)
+  (let ((dispatch-table (create-table)))
+    (values (lambda (extension-symbol f) ; extend
+              (put-in-table dispatch-table extension-symbol f))
+            (lambda (. args) ; invoke
+              (let* ((dispatch (apply dispatch-lambda args))
+                     (method (get-from-table dispatch-table dispatch)))
+                (apply method args))))))
