6a2dc53dc8351e7396e7376a8a62233dc81a22ae
Justin Glenn Smith <justin.smith@fundingcircle.com>
2020-07-03 12:24:19 -0700

    paying with various assembly stuff, arm via qemu

--- 8-arm-assembly/0-pc-demo.s
+++ 8-arm-assembly/0-pc-demo.s
@@ -1,8 +1,14 @@
 .section .text
 .global _start
 
-_start:
-  mov r0, pc
-  mov r1, #2
-  add r2, r1, r1
-  bkpt
+_start: mov r0, pc
+	mov r1, #2
+	add r2, r1, r1
+
+	mov r0, r2 /* exit with the value of r2 */
+	b _exit
+
+_exit:
+	mov r7, #1 /* system call register, exit index */
+	swi #0
+	
--- 8-arm-assembly/life.s
+++ 8-arm-assembly/life.s
@@ -1,3 +1,15 @@
 ; the game of LIFE by conway, represented in ARM assembler
 ;
 
+;;; example, loading and storing memory
+	 mov		r1, #0x00000290
+	 ;		since 666 won't fit in a single numeric literal
+	 add		r1, r1, #0x0000000a
+	 ;		store 666 into random memory location
+	 mov		r2, #0x00011000
+	 str		r1, [r2]
+	 ;		load 666 from that same location
+	 ldr		r0, [r2]
+
+_output_cell:
+brk
