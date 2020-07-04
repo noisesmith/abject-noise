8320256571a4c15949078fd17556f973d4526ce0
Justin Glenn Smith <justin.smith@fundingcircle.com>
2020-07-04 09:24:44 -0700

    better multi abstraction definition
    class-of should be safe to use here without incurring the cost of other
    goops abstractions

--- 6-scheme-4-2020/scm/noisesmith/oo.scm
+++ 6-scheme-4-2020/scm/noisesmith/oo.scm
@@ -1,25 +1,10 @@
 (define-module (noisesmith oo)
-               #:export (->thing)
+               #:export (defmulti type)
                #:use-module ((ice-9 match)
                              #:select ()
-                             #:renamer (symbol-prefix-proc 'match:)))
-
-(define-syntax _thing_match
-  (syntax-rules ()
-    ((_thing_match m k v)
-     (if (eq? m k)
-         v))
-    ((_thing_match m k v kvs ...)
-     (if (eq? m k)
-         v
-         (_thing_match m kvs ...)))))
-
-(define-syntax ->thing
-  (syntax-rules ()
-    ((->thing k v kvs ...)
-     (lambda (. args)
-       (_thing_match (car args)
-                     k v kvs ...)))))
+                             #:renamer (symbol-prefix-proc 'match:))
+               #:use-module ((oo goops)
+                             #:select (class-of)))
 
 (define (create-table)
   (make-hash-table))
@@ -31,15 +16,20 @@
 (define (put-in-table t k v)
   (hash-set! t k v))
 
+;; looks like this goops function might be my best bet here
+(define type class-of)
+
 ;; TODO: this is almost it - it is close to clojure's defmulti, without the
 ;; binding part, and without the heirarchic lookup of dispatch built in
 ;; the inheriting dispatch can be a wrapper for the dispatch-lambda
 ;; would a separate version that only dispatches on the first arg's type be an optimization?
-(define (defmulti dispatch-lambda)
-  (let ((dispatch-table (create-table)))
-    (values (lambda (extension-symbol f) ; extend
-              (put-in-table dispatch-table extension-symbol f))
-            (lambda (. args) ; invoke
-              (let* ((dispatch (apply dispatch-lambda args))
-                     (method (get-from-table dispatch-table dispatch)))
-                (apply method args))))))
+(define (multi . dispatch-lambda)
+  (let ((dispatch-lambda (or (car dispatch-lambda)
+                             type))
+        (dispatch-table (create-table)))
+    `(,(lambda (extension-symbol f) ; extend
+         (put-in-table dispatch-table extension-symbol f))
+      ,(lambda (. args) ; invoke
+         (let* ((dispatch (apply dispatch-lambda args))
+                (method (get-from-table dispatch-table dispatch)))
+           (apply method args))))))
