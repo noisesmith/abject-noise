f31e7544b0ff2c74cc826a4f5719353eb087f2ea
Justin Glenn Smith <justin.smith@fundingcircle.com>
2021-04-09 10:47:49 -0700

    transition to lower level input language

--- 11-dsl/src/org/noisesmith/composition_dsl/composition.clj
+++ 11-dsl/src/org/noisesmith/composition_dsl/composition.clj
@@ -1,8 +1,11 @@
 (ns org.noisesmith.composition-dsl.composition
-  (:require [org.noisesmith.composition-dsl.emit :as emit]))
+  (:require [org.noisesmith.composition-dsl.emit :as emit]
+            [org.noisesmith.composition-dsl.operations :as op]
+            [org.noisesmith.composition-dsl.generate :as gen]))
 
 ;; TODO - make a DSL targetting the features in the operations namespace
 ;; TODO - use the tables namespace
+#_ ;; the redef below should be equivalent to this...
 (def score
   [{:i 1
     :start 0
@@ -12,10 +15,37 @@
              :amp [[0 0.1 1 1 1 1 1 1 0.1 0]]
              :hz [[1000 1000]]}}])
 
+(def score
+  [[:i 1 :amp :hz]
+   [:p :amp 0]
+   [:p :hz 1000]
+   [:t 0.1]
+   [:p :amp 1]
+   [:t 0.8]
+   [:p :amp 1]
+   [:t 0.1]
+   [:p :amp 0]
+   [:p :hz 1000]
+   [:e]])
+
+(defn spy
+  [prefix data]
+  (binding [*out* *err*]
+    (println prefix (pr-str data)))
+  data)
+
 (defn -main
   [& _]
   (->> score
+       (spy "original input")
+       (reduce op/input-dispatch op/empty-composition)
+       (spy "from op")
+       (:events)
+       (reduce gen/event-collect gen/empty-score)
+       (spy "from gen")
+       (:instruments)
        (reduce emit/event-emit emit/empty-state)
+       (spy "from emit")
        (emit/to-sco)
        (run! (fn [v]
                (apply println v)))))
